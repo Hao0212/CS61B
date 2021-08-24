430601b6c5084f32b90cc191f707cf8430446fb4
zhouhao <240985032@qq.com>
2021-08-24 17:42:05 +0800

    Completed first part of lab1

---
+++ lab1/HelloWorldTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author Hao
+ * @date 2021/8/7 - 11:25 PM
+ */
+public class HelloWorldTest {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+}
